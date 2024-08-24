4c2c1d7ae53442c877600797593c7d2cdf22b938
RafatMeraz <razhossain201@gmail.com>
2024-08-24 23:43:52 +0600

    Add basic data types and hello world code

---
+++ basics/data_types.dart
@@ -0,0 +1,25 @@
+// this program is for intro to data types - single line comment
+/*
+multiline comment
+jdk d
+dfkdjf d''
+'dkfjdkjfd'
+'askjdsk'
+jok
+klk
+*/
+main() {
+  // integer - 1, 2, 2893493490, 1092093920390 -> int
+  // float - 3.1416, 10.45, 3423434.453 -> double
+  // boolean - true/false -> bool
+  // text - name, address -> String
+  print(123);
+  print(3.1416);
+  print('Ostad Limited');
+  print(true);
+  print(false);
+  print(12873873278-3434);
+  print(12873873278+3434);
+  print(12873873278/3434);
+  print(12873873278*3434);
+}
