932cccf4fb5daa23cfafae513095d7d40712a9a3
RafatMeraz <razhossain201@gmail.com>
2024-09-30 23:18:39 +0600

    Add Center and Text widget

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
+// reformat
+// ctl + shift + l // cmd + shift + l
+
+// suggestion
+// ctl + space
+
+// var, dynamic
+
 void main() {
-  runApp(MyApp());
+  runApp(HelloWorldApp());
 }
 
-class MyApp extends StatelessWidget {
+class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      debugShowCheckedModeBanner: true,
+      title: 'Hello World App',
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: Text('Home'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'Hello World we are writing our first application code with flutter',
+            textAlign: TextAlign.justify,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+              letterSpacing: 0.8,
+              height: 1.5,
+              // backgroundColor: Colors.black,
+              wordSpacing: 1,
+              // decoration: TextDecoration.lineThrough,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
