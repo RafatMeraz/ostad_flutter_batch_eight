2a07f3fd97127781556d9a74d047ecbf1e50094b
RafatMeraz <razhossain201@gmail.com>
2024-10-09 23:24:53 +0600

    Add Divider, ModalBottomSheet, TextField

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Image(asset, network), Column, Row
+// Divider, ModalBottomSheet, TextField
 
 void main() {
   runApp(HelloWorldApp());
@@ -33,104 +33,113 @@ class Home extends StatelessWidget {
           children: [
             SizedBox(height: 100),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-                foregroundColor: Colors.white,
-                textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
-                padding: EdgeInsets.symmetric(horizontal: 48, vertical: 16),
-                shadowColor: Colors.amber,
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                  // side: BorderSide(color: Colors.green, width: 4)
-                ),
-                side: BorderSide(color: Colors.green, width: 4),
-                minimumSize: Size(100, 40),
-                // maximumSize: Size(300, 100),
-              ),
               onPressed: () {
-                showDialog(
-                  barrierColor: Colors.green,
-                  barrierDismissible: false,
+                // showAboutDialog(context: context);
+                showModalBottomSheet(
+                  // barrierColor: Colors.grey.shade50,
+                  // backgroundColor: Colors.green.shade200,
                   context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text('Delete'),
-                      content: Text('Are you sure?'),
-                      actions: [
-                        TextButton(onPressed: () {}, child: Text('No')),
-                        TextButton(onPressed: () {}, child: Text('Yes')),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  isScrollControlled: true,
+                  useSafeArea: true,
+                  enableDrag: false,
+                  builder: (ctx) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Title',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Divider(
+                          height: 20,
+                          thickness: 4,
+                        ),
+                        Text('Sample'),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {}, child: Text('Cancel')),
+                            ElevatedButton(
+                                onPressed: () {}, child: Text('Save')),
+                          ],
+                        )
                       ],
                     );
                   },
                 );
               },
-              child: Text('Tap'),
+              child: Text('Show dialog'),
             ),
-            SizedBox(height: 16),
-            TextButton(
-              style: TextButton.styleFrom(),
-              onPressed: () {
-                print('Tapped text button');
-              },
-              child: Text('Tap here'),
-            ),
-            SizedBox(height: 16),
-            IconButton(
-              style: IconButton.styleFrom(),
-              onPressed: () {
-                print('Tapped text button');
-              },
-              icon: Icon(Icons.add),
-            ),
-            SizedBox(height: 16),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(),
-              onPressed: () {
-                print('Tapped text button');
-              },
-              child: Text('Click here'),
-            ),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                print('Just one click');
-              },
-              onDoubleTap: () {
-                print('Double tapped');
-              },
-              onLongPress: () {
-                print('On long press');
-              },
-              onLongPressCancel: () {
-                print('On long press cancel');
-              },
-              onLongPressEnd: (details) {
-                print('On long press end');
-              },
-              child: Column(
-                children: [
-                  Text('Simple Text'),
-                  Text('Simple Text'),
-                  Text('Simple Text'),
-                  Text('Simple Text'),
-                ],
+            SizedBox(height: 24),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                maxLength: 50,
+                onChanged: (String? value) {
+                  print(value);
+                },
+                controller: TextEditingController(),
+                // obscureText: true,
+                maxLines: 1,
+                keyboardType: TextInputType.phone,
+                enabled: true,
+                decoration: InputDecoration(
+                  hintText: 'Phone',
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey.shade400,
+                  ),
+                  labelText: 'Phone number',
+                  // label: Icon(Icons.add),
+                  prefixIcon: Icon(Icons.phone),
+                  suffixIcon: Icon(Icons.person),
+                  fillColor: Colors.white54,
+                  filled: true,
+                  // counterStyle: TextStyle(
+                  //   fontSize: 24
+                  // ),
+                  // counterText: '',
+                  // prefix: Column(
+                  //   children: [
+                  //     Text('d'),
+                  //     Text('b'),
+                  //   ],
+                  // ),
+                  // suffix: Column(
+                  //   children: [
+                  //     Text('d'),
+                  //     Text('b'),
+                  //   ],
+                  // ),
+                  // icon: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green, width: 2),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 4),
+                  ),
+                ),
               ),
             ),
-            InkWell(
-              splashColor: Colors.green,
-              onTap: () {
-                print('Ink well');
-              },
-              child: Text('Behave like button'),
-            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
       ),
     );
   }
 }
+
+// Suggestion => ctl + space
