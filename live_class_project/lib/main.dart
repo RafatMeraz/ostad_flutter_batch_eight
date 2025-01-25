1d5f9b795a1a9f153629e3a80c905592b242a0ea
RafatMeraz <razhossain201@gmail.com>
2025-01-25 23:13:39 +0600

    Add getx counter app

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:live_class_project/app.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const CRUDApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  RxInt count = 0.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: Obx(() {
+          return Text(
+            count.toString(),
+            style: const TextStyle(fontSize: 32),
+          );
+        })
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          count.value++;
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
