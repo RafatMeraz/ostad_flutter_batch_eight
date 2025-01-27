2787eb59f414cbdcdb89b3228bd2e70d6bfaf8e9
RafatMeraz <razhossain201@gmail.com>
2025-01-27 22:54:01 +0600

    Add getx route management

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -10,8 +10,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return GetMaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: (RouteSettings settings) {
+        late Widget routeWidget;
+
+        if (settings.name == '/') {
+          routeWidget = const HomeScreen();
+        } else if (settings.name == '/profile') {
+          routeWidget = const ProfileScreen();
+        } else if (settings.name == '/settings') {
+          routeWidget = const SettingsScreen();
+        }
+
+        return MaterialPageRoute(builder: (context) {
+          return routeWidget;
+        });
+      },
     );
   }
 }
@@ -31,18 +46,98 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: Center(
-        child: Obx(() {
-          return Text(
-            count.toString(),
-            style: const TextStyle(fontSize: 32),
-          );
-        })
-      ),
+          child: Column(
+        children: [
+          Obx(() {
+            return Text(
+              count.toString(),
+              style: const TextStyle(fontSize: 32),
+            );
+          }),
+          TextButton(
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const SettingsScreen()),
+              // );
+              // Get.to(const SettingsScreen());
+              Get.toNamed('/settings');
+            },
+            child: const Text('Go to Settings'),
+          )
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           count.value++;
         },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings')),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                // Navigator.pushReplacement(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => const ProfileScreen(),
+                //   ),
+                // );
+                // Get.off(const ProfileScreen());
+                Get.offNamed('/profile');
+              },
+              child: const Text('Go to Profile'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Navigator.pop(context);
+                Get.back();
+              },
+              child: const Text('Back'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                // Navigator.pushAndRemoveUntil(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                //   (predicate) => false,
+                // );
+                // Get.offAll(const HomeScreen());
+                Get.offAllNamed('/');
+              },
+              child: const Text('Go to Home'),
+            )
+          ],
+        ),
       ),
     );
   }
